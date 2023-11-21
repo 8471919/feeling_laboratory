092fca8a7605a8334d2528291ffb0372abadfd7f
Michael <7471919@naver.com>
2023-11-21 12:21:06 +0900

    test: AnswerQuestionnaire e2e Test Todo 작성

---
+++ src/test/e2e/answer-questionnaire.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('e2e AnswerQuestionnaire Test', () => {
+  let app: INestApplication;
+  let testingModule: TestingModule;
+
+  const baseUrl = '/api/answer-questionnaire';
+
+  beforeAll(async () => {
+    testingModule = await Test.createTestingModule({
+      imports: [AnswerQuestionnaireModule],
+    }).compile();
+
+    app = testingModule.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('POST /api/answer-questionnaire', () => {
+    test.todo('답변 설문지가 정상적으로 생성되는지 검증');
+  });
+
+  describe('GET /api/answer-questionnaire', () => {
+    test.todo('답변 설문지가 정상적으로 읽어지는지 검증');
+  });
+
+  describe('PUT /api/answer-questionnaire', () => {
+    test.todo('답변 설문지가 정상적으로 수정되는지 검증');
+  });
+
+  describe('DELETE /api/answer-questionnaire', () => {
+    test.todo('답변 설문지가 정상적으로 삭제되는지 검증');
+  });
+});

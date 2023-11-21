c195dcd49f5d83e9df8e19317f5b128257c2f309
Michael <7471919@naver.com>
2023-11-21 11:50:53 +0900

    test: QuestionOption e2e Test Todo 작성

---
+++ src/test/e2e/question-option.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('e2e QuestionOption Test', () => {
+  let app: INestApplication;
+  let testingModule: TestingModule;
+
+  const baseUrl = '/api/question-option';
+
+  beforeAll(async () => {
+    testingModule = await Test.createTestingModule({
+      imports: [QuestionOptionModule],
+    }).compile();
+
+    app = testingModule.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('POST /api/question-option', () => {
+    test.todo('선택지가 정상적으로 생성되는지 검증');
+  });
+
+  describe('GET /api/question-option', () => {
+    test.todo('선택지가 정상적으로 읽어지는지 검증');
+  });
+
+  describe('PUT /api/question-option', () => {
+    test.todo('선택지가 정상적으로 수정되는지 검증');
+  });
+
+  describe('DELETE /api/question-option', () => {
+    test.todo('선택지가 정상적으로 삭제되는지 검증');
+  });
+});

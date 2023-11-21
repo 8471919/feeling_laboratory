7c906d12605168ef701572c738e7eea105d64ef1
Michael <7471919@naver.com>
2023-11-21 12:23:03 +0900

    test: AnswerQuestion e2e Test Todo 작성

---
+++ src/test/e2e/answer-question.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('e2e AnswerQuestion Test', () => {
+  let app: INestApplication;
+  let testingModule: TestingModule;
+
+  const baseUrl = '/api/answer-question';
+
+  beforeAll(async () => {
+    testingModule = await Test.createTestingModule({
+      imports: [AnswerQuestionModule],
+    }).compile();
+
+    app = testingModule.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('POST /api/answer-question', () => {
+    test.todo('설문 답변 문항이 정상적으로 생성되는지 검증');
+  });
+
+  describe('GET /api/answer-question', () => {
+    test.todo('설문 답변 문항이 정상적으로 읽어지는지 검증');
+  });
+
+  describe('PUT /api/answer-question', () => {
+    test.todo('설문 답변 문항이 정상적으로 수정되는지 검증');
+  });
+
+  describe('DELETE /api/answer-question', () => {
+    test.todo('설문 답변 문항이 정상적으로 삭제되는지 검증');
+  });
+});

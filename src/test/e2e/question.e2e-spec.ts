1cb0c5000defbbd278637dc39c09108bd4b6f59c
Michael <7471919@naver.com>
2023-11-22 20:59:14 +0900

    test: 설문 문항이 정상적으로 수정되는지 검증 Test 작성

--- src/test/e2e/question.e2e-spec.ts
+++ src/test/e2e/question.e2e-spec.ts
@@ -2,6 +2,7 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
 import { CreateQuestionDto } from 'src/dtos/question/create-question.dto';
+import { UpdateQuestionDto } from 'src/dtos/question/update-question.dto';
 import { CreateQuestionnaireDto } from 'src/dtos/questionnaire/create-questionnaire.dto';
 import { ERROR_MESSAGE } from 'src/utils/constants/error-message';
 import * as request from 'supertest';
@@ -13,6 +14,7 @@ describe('e2e Question Test', () => {
   const baseUrl = '/api/question';
 
   let questionnaireId;
+  let questionId;
 
   beforeAll(async () => {
     testingModule = await Test.createTestingModule({
@@ -59,6 +61,8 @@ describe('e2e Question Test', () => {
         .post(baseUrl)
         .send(questionInfo);
 
+      questionId = res.body?.data?.id;
+
       expect(res.body.data).toHaveProperty('content');
     });
   });
@@ -82,7 +86,18 @@ describe('e2e Question Test', () => {
   });
 
   describe('PUT /api/question', () => {
-    test.todo('설문 문항이 정상적으로 수정되는지 검증');
+    test('설문 문항이 정상적으로 수정되는지 검증', async () => {
+      const questionInfo: UpdateQuestionDto = {
+        content: 'updated question',
+        order: 1,
+      };
+
+      const res = await request(app.getHttpServer())
+        .put(`${baseUrl}/${questionId}`)
+        .send(questionInfo);
+
+      expect(res.body.data).toBe(true);
+    });
   });
 
   describe('DELETE /api/question', () => {

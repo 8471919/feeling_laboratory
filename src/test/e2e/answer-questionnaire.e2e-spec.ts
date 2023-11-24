8cbb5b66545e624db8a17f37e3b4bf9f8e1ac5c1
Michael <7471919@naver.com>
2023-11-24 23:40:38 +0900

    test: 답변 설문지가 정상적으로 생성되는지 검증 Test 작성

--- src/test/e2e/answer-questionnaire.e2e-spec.ts
+++ src/test/e2e/answer-questionnaire.e2e-spec.ts
@@ -1,12 +1,21 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
+import { CreateAnswerQuestionnaireDto } from 'src/dtos/answer-questionnaire/create-answer-questionnaire.dto';
+import { CreateQuestionOptionEntityDto } from 'src/dtos/question-option/create-question-option.dto';
+import { CreateQuestionDto } from 'src/dtos/question/create-question.dto';
+import { CreateQuestionnaireDto } from 'src/dtos/questionnaire/create-questionnaire.dto';
+import * as request from 'supertest';
 
 describe('e2e AnswerQuestionnaire Test', () => {
   let app: INestApplication;
   let testingModule: TestingModule;
 
   const baseUrl = '/api/answer-questionnaire';
+
+  let questionnaireId;
+  let questionId;
+  let questionOptionId;
 
   beforeAll(async () => {
     testingModule = await Test.createTestingModule({
@@ -15,6 +24,51 @@ describe('e2e AnswerQuestionnaire Test', () => {
 
     app = testingModule.createNestApplication();
     await app.init();
+
+    // 테스트를 위한 설문지 생성
+    const startedAt = new Date();
+    const finishedAt = new Date(
+      startedAt.getFullYear(),
+      startedAt.getMonth(),
+      startedAt.getDate() + 1
+    );
+
+    const questionnaireInfo: CreateQuestionnaireDto = {
+      author: 'author',
+      title: 'title',
+      startedAt,
+      finishedAt,
+    };
+    const questionnaire = await request(app.getHttpServer())
+      .post('/api/questionnaire')
+      .send(questionnaireInfo);
+
+    questionnaireId = questionnaire.body?.data?.id;
+
+    // 테스트를 위한 설문 문항 생성
+    const questionInfo: CreateQuestionDto = {
+      content: 'test content',
+      order: 1,
+      questionnaireId,
+    };
+
+    const question = await request(app.getHttpServer())
+      .post('/api/question')
+      .send(questionInfo);
+
+    questionId = question.body?.data?.id;
+
+    // 테스트를 위한 문항 선택지 생성
+    const questionOptionInfo: CreateQuestionOptionEntityDto = {
+      content: 'test option content',
+      order: 1,
+      questionId,
+      score: 3,
+    };
+
+    const questionOption = await request(app.getHttpServer())
+      .post('/api/question-option')
+      .send(questionOptionInfo);
   });
 
   afterAll(async () => {
@@ -22,7 +76,19 @@ describe('e2e AnswerQuestionnaire Test', () => {
   });
 
   describe('POST /api/answer-questionnaire', () => {
-    test.todo('답변 설문지가 정상적으로 생성되는지 검증');
+    test('답변 설문지가 정상적으로 생성되는지 검증', async () => {
+      const answerQuestionInfo: CreateAnswerQuestionnaireDto = {
+        questionnaireId: 1,
+      };
+
+      const res = await request(app.getHttpServer())
+        .post(baseUrl)
+        .send(answerQuestionInfo);
+
+      expect(res.body.data.questionnaireId).toStrictEqual(
+        answerQuestionInfo.questionnaireId
+      );
+    });
   });
 
   describe('GET /api/answer-questionnaire', () => {

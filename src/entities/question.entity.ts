041e33090927c21c276f18a183ea5f0507b1c980
Michael <7471919@naver.com>
2023-11-25 15:44:29 +0900

    docs: QuestionEntity에 ApiProperty 추가

--- src/entities/question.entity.ts
+++ src/entities/question.entity.ts
@@ -4,18 +4,34 @@ import { QuestionnaireEntity } from './questionnaire.entity';
 import { QuestionOptionEntity } from './question-option.entity';
 import { AnswerQuestionEntity } from './answer-question.entity';
 import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('Question')
 export class QuestionEntity extends CommonIntPKEntity {
+  @ApiProperty({
+    example: '다음 중 어느 곳을 여행가고 싶나요? (1개만 선택)',
+    description: '질문 내용',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   @Column('text', { unique: false, nullable: false })
   content: string;
 
+  @ApiProperty({
+    example: 1,
+    description: '설문 문항 순서',
+    required: true,
+  })
   @IsInt()
   @Column('int', { unique: false, nullable: false })
   order: number;
 
+  @ApiProperty({
+    example: 1,
+    description: '설문지 id',
+    required: true,
+  })
   @IsInt()
   @Column('int', { unique: false, nullable: false })
   questionnaireId: number;

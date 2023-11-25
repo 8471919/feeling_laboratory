e135f094a0714415b51f86618aabf6b3c18c1ec8
Michael <7471919@naver.com>
2023-11-25 16:31:41 +0900

    docs: AnswerQuestionnaire Swagger 작성

--- src/res/answer-questionnaire/answer-questionnaire.controller.ts
+++ src/res/answer-questionnaire/answer-questionnaire.controller.ts
@@ -11,13 +11,19 @@ import {
 import { AnswerQuestionnaireService } from './answer-questionnaire.service';
 import { FindAnswerQuestionnaireDto } from 'src/dtos/answer-questionnaire/find-answer-questionnaire.dto';
 import { CreateAnswerQuestionnaireDto } from 'src/dtos/answer-questionnaire/create-answer-questionnaire.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('답변 API')
 @Controller('/api/answer-questionnaire')
 export class AnswerQuestionnaireController {
   constructor(
     private readonly answerQuestionnaireService: AnswerQuestionnaireService
   ) {}
 
+  @ApiOperation({
+    summary: '답변 설문지 Create',
+    description: '설문지에 대한 답변 설문지 생성(답변 생성은 따로 API가 존재)',
+  })
   @Post()
   async createAnswerQuestionnaire(
     @Body() body: CreateAnswerQuestionnaireDto
@@ -28,6 +34,10 @@ export class AnswerQuestionnaireController {
     return answerQuestionnaire;
   }
 
+  @ApiOperation({
+    summary: '답변 설문지 목록 Read',
+    description: '해당하는 설문지에 대한 답변 설문지 목록을 읽어온다.',
+  })
   @Get('/list')
   async getAnswerQuestionnaireList(
     @Query('questionnaireId', new ParseIntPipe()) questionnaireId: number
@@ -40,6 +50,10 @@ export class AnswerQuestionnaireController {
     return answerQuestionnaireList;
   }
 
+  @ApiOperation({
+    summary: '답변 Read / 완료된 설문지 확인',
+    description: '설문지에 대한 답변들을 읽어온다.',
+  })
   @Get('/:id')
   async getAnswerQuestionnaire(
     @Param('id', new ParseIntPipe()) answerQuestionnaireId: number
@@ -52,6 +66,10 @@ export class AnswerQuestionnaireController {
     return answerQuestionnaire;
   }
 
+  @ApiOperation({
+    summary: '답변 Delete',
+    description: '답변 설문지를 삭제한다.',
+  })
   @Delete('/:id')
   async removeAnswerQuestionnaire(
     @Param('id', new ParseIntPipe()) answerQuestionnaireId: number

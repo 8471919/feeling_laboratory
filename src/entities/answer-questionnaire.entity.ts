d9c0bb83d8bd53de74dd5ea9818073d6e664b0dc
Michael <7471919@naver.com>
2023-11-25 15:40:22 +0900

    docs: AnswerQuestionnaire에 ApiProperty 추가

--- src/entities/answer-questionnaire.entity.ts
+++ src/entities/answer-questionnaire.entity.ts
@@ -3,9 +3,15 @@ import { CommonIntPKEntity } from './common/common.entity';
 import { QuestionnaireEntity } from './questionnaire.entity';
 import { AnswerQuestionEntity } from './answer-question.entity';
 import { IsInt } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('AnswerQuestionnaire')
 export class AnswerQuestionnaireEntity extends CommonIntPKEntity {
+  @ApiProperty({
+    example: 1,
+    description: '설문지 id',
+    required: true,
+  })
   @IsInt()
   @Column('int', { unique: false, nullable: false })
   questionnaireId: number;

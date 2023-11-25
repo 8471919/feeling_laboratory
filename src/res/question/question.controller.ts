41473ce7124d9917f245a57406234b6b5d082d76
Michael <7471919@naver.com>
2023-11-25 16:15:36 +0900

    docs: Question Swagger 작성

--- src/res/question/question.controller.ts
+++ src/res/question/question.controller.ts
@@ -13,11 +13,17 @@ import { QuestionService } from './question.service';
 import { CreateQuestionDto } from 'src/dtos/question/create-question.dto';
 import { FindQuestionDto } from 'src/dtos/question/find-question.dto';
 import { UpdateQuestionDto } from 'src/dtos/question/update-question.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('문항 API')
 @Controller('/api/question')
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
 
+  @ApiOperation({
+    summary: '문항 Create',
+    description: '설문 문항을 생성한다.',
+  })
   @Post()
   async createQuestion(
     @Body() body: CreateQuestionDto
@@ -27,6 +33,10 @@ export class QuestionController {
     return question;
   }
 
+  @ApiOperation({
+    summary: '문항 Read',
+    description: '설문 문항 리스트를 읽어온다.',
+  })
   @Get('/list')
   async getQuestionList(
     @Query('questionnaireId', new ParseIntPipe()) questionnaireId: number
@@ -38,6 +48,10 @@ export class QuestionController {
     return questionList;
   }
 
+  @ApiOperation({
+    summary: '문항 Update',
+    description: '설문 문항을 수정한다.',
+  })
   @Put('/:id')
   async modifyQuestion(
     @Body() body: UpdateQuestionDto,
@@ -51,6 +65,10 @@ export class QuestionController {
     return isUpdated;
   }
 
+  @ApiOperation({
+    summary: '문항 Delete',
+    description: '설문 문항을 삭제한다.',
+  })
   @Delete('/:id')
   async removeQuestion(@Param('id', new ParseIntPipe()) questionId: number) {
     const isDeleted = await this.questionService.removeQuestion(questionId);

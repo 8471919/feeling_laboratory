940f4ba68ea02e261421c5b676f59ba04ce835fb
Michael <7471919@naver.com>
2023-11-25 15:39:55 +0900

    docs: AnswerQuetionEntity에 ApiProperty 추가

--- src/entities/answer-question.entity.ts
+++ src/entities/answer-question.entity.ts
@@ -5,17 +5,33 @@ import { QuestionEntity } from './question.entity';
 import { QuestionOptionEntity } from './question-option.entity';
 import { IsInt } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('AnswerQuestion')
 export class AnswerQuestionEntity extends CommonBigIntPKEntity {
+  @ApiProperty({
+    example: 1,
+    description: '답변 설문지 id',
+    required: true,
+  })
   @IsInt()
   @Column('int', { unique: false, nullable: false })
   answerQuestionnaireId: number;
 
+  @ApiProperty({
+    example: 1,
+    description: '설문 문항 id',
+    required: true,
+  })
   @IsInt()
   @Column('int', { unique: false, nullable: false })
   questionId: number;
 
+  @ApiProperty({
+    example: 1,
+    description: '설문 문항 선택지 id',
+    required: true,
+  })
   @Type(() => Number)
   @IsInt()
   @Type(() => String)

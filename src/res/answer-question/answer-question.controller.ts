77d9fe5ac7ccc4e74f5baf2aefed37b1646d7ecc
Michael <7471919@naver.com>
2023-11-25 16:38:59 +0900

    docs: AnswerQuestion Swagger 작성

--- src/res/answer-question/answer-question.controller.ts
+++ src/res/answer-question/answer-question.controller.ts
@@ -3,11 +3,17 @@ import { AnswerQuestionService } from './answer-question.service';
 import { FindAnswerQuestionDto } from 'src/dtos/answer-question/find-answer-question.dto';
 import { CreateAnswerQuestionDto } from 'src/dtos/answer-question/create-answer-question.dto';
 import { UpdateAnswerQuestionDto } from 'src/dtos/answer-question/update-answer-question.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('답변 API')
 @Controller('/api/answer-question')
 export class AnswerQuestionController {
   constructor(private readonly answerQuestionService: AnswerQuestionService) {}
 
+  @ApiOperation({
+    summary: '답변 Create',
+    description: '설문 문항에 대한 답변을 생성한다.',
+  })
   @Post()
   async createAnswerQuestion(
     @Body() body: CreateAnswerQuestionDto
@@ -18,6 +24,10 @@ export class AnswerQuestionController {
     return answerQuestion;
   }
 
+  @ApiOperation({
+    summary: '답변 Update',
+    description: '답변을 수정한다.',
+  })
   @Put('/:id')
   async modifyAnswerQuestion(
     @Body() body: UpdateAnswerQuestionDto,

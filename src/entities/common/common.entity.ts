65556a1d7840719323ee9cfb4ee18065018450ba
Michael <7471919@naver.com>
2023-11-25 15:36:50 +0900

    docs: CommonEntity에 ApiProperty 추가

--- src/entities/common/common.entity.ts
+++ src/entities/common/common.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsDateString, IsInt, IsOptional } from 'class-validator';
 import {
@@ -8,18 +9,38 @@ import {
 } from 'typeorm';
 
 export class CommonIntPKEntity {
+  @ApiProperty({
+    example: 1,
+    description: 'id',
+    required: true,
+  })
   @IsInt()
   @PrimaryGeneratedColumn({ type: 'int' })
   id: number;
 
+  @ApiProperty({
+    example: new Date().toISOString(),
+    description: '생성 일자',
+    required: true,
+  })
   @IsDateString()
   @CreateDateColumn({ type: 'timestamp', nullable: false })
   createdAt: Date;
 
+  @ApiProperty({
+    example: new Date().toISOString(),
+    description: '수정 일자',
+    required: true,
+  })
   @IsDateString()
   @UpdateDateColumn({ type: 'timestamp', nullable: false })
   updatedAt: Date;
 
+  @ApiProperty({
+    example: new Date().toISOString(),
+    description: '삭제 일자',
+    required: false,
+  })
   @IsDateString()
   @IsOptional()
   @DeleteDateColumn({ type: 'timestamp', nullable: true })
@@ -27,20 +48,40 @@ export class CommonIntPKEntity {
 }
 
 export class CommonBigIntPKEntity {
+  @ApiProperty({
+    example: '1',
+    description: 'id(bigint)',
+    required: true,
+  })
   @Type(() => Number)
   @IsInt()
   @Type(() => String)
   @PrimaryGeneratedColumn({ type: 'bigint' })
   id: string;
 
+  @ApiProperty({
+    example: new Date().toISOString(),
+    description: '생성 일자',
+    required: true,
+  })
   @IsDateString()
   @CreateDateColumn({ type: 'timestamp', nullable: false })
   createdAt: Date;
 
+  @ApiProperty({
+    example: new Date().toISOString(),
+    description: '수정 일자',
+    required: true,
+  })
   @IsDateString()
   @UpdateDateColumn({ type: 'timestamp', nullable: false })
   updatedAt: Date;
 
+  @ApiProperty({
+    example: new Date().toISOString(),
+    description: '삭제 일자',
+    required: false,
+  })
   @IsDateString()
   @IsOptional()
   @DeleteDateColumn({ type: 'timestamp', nullable: true })
